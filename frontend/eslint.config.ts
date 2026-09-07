ee2159de4c24a35f966c10d3e4c1a3d6611d1c7f
Jesus-Velazquez-mx <jesu10137@gmai.com>
2026-09-07 13:43:50 -0700

    Se agregaron las credenciales del usuario que SOLO puede usar la AWS CLI para S3. Se cambió el linter a ESlint. Se probó el funcionamiento de los objetos del bucket.

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  {
+    languageOptions: { globals: globals.browser },
+    settings: { react: { version: "19.2.8" } }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"]
+];
