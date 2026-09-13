673d38896f3b14cafecc39d49eb5edbc62cc2fcf
KevinSamano3321 <L22170815@culiacan.tecnm.mx>
2026-09-13 11:16:12 -0700

    Correciones backend

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -11,7 +11,7 @@ const registerSchema = z.object({
     name: z.string().min(2),
     lastname: z.string().min(2),
     phone: z.string().max(10).optional(),
-    email: z.string().email(),
+    email: z.email(),
     password: z.string().min(6)
 });
 
