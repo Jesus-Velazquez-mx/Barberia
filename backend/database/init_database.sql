f01d2f005c88032d4d0a3de752df5814aee82ab6
Lihn2254 <erick.davidhf@gmail.com>
2026-09-24 15:19:38 -0700

    Agregar campo "turno" (shifts) a tabla de recepcionistas

--- backend/database/init_database.sql
+++ backend/database/init_database.sql
@@ -177,6 +177,7 @@ CREATE TRIGGER trg_barbers_check_role BEFORE INSERT OR UPDATE ON barbers
 CREATE TABLE receptionists (
     user_id     uuid PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     shop_id     uuid NOT NULL REFERENCES shops(id) ON DELETE RESTRICT, -- receptionist works at one specific shop
+    shift_id    uuid NOT NULL REFERENCES shifts(id) ON DELETE RESTRICT,
     deleted_at  timestamptz
 );
 
